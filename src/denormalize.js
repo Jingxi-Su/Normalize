3d8806cfa88c0b73e7ef394dae68ea6b65fb5b71
Jingxi Su <jingxi.su@shopee.com>
2022-07-19 12:08:24 +0800

    update some notes

--- src/denormalize.js
+++ src/denormalize.js
@@ -1,119 +1,119 @@
-import EntitySchema from "./entity.js";
+import EntitySchema from './entity.js'
 
-// 类比normalize中的flatten
+// 类比normalize中的flatten，由外向内递归
 const getUnflatten = (entities) => {
-    const cache = {};
-    const getEntity = getEntities(entities);
-    return function unflatten (data, schema) {
-        if (typeof schema.getName === 'undefined') {
-            return unflattenNoEntity(schema, data, unflatten);
-        }
-        return unflattenEntity(data, schema, unflatten, getEntity, cache);
-    };
-};
+  const cache = {}
+  const getEntity = getEntities(entities)
+  return function unflatten (data, schema) {
+    if (typeof schema.getName === 'undefined') {
+      return unflattenNoEntity(schema, data, unflatten)
+    }
+    return unflattenEntity(data, schema, unflatten, getEntity, cache)
+  }
+}
 
 // 针对schema是一个EntitySchema实体的情况
 const unflattenEntity = (id, schema, unflatten, getEntity, cache) => {
-    const entity = getEntity(id, schema);
-    if (!cache[schema.getName()]) {
-        cache[schema.getName()] = {}
-    }
-    if (!cache[schema.getName()][id]) {
-        const entityCopy = { ...entity };
-        //递归的方法，存在schema嵌套的情况下要一级接着一级的往下递归到根部
-        Object.keys(schema.schema).forEach((key) => {
-            if (entityCopy.hasOwnProperty(key)) {
-                const uschema = schema.schema[key];
-                entityCopy[key] = unflatten(entityCopy[key], uschema);
-            }
-        });
-        cache[schema.getName()][id] = entityCopy;
-    }
-    return cache[schema.getName()][id];
-};
+  const entity = getEntity(id, schema)// 取到的对应schema的某个id所对应的对象
+  if (!cache[schema.getName()]) {
+    cache[schema.getName()] = {}
+  }
+  if (!cache[schema.getName()][id]) {
+    const entityCopy = { ...entity }
+    // 在有嵌套的情况下递归,由外向内
+    Object.keys(schema.schema).forEach((key) => {
+      if (entityCopy.hasOwnProperty(key)) {
+        const innerschema = schema.schema[key]
+        // 范式化数据替换回原本数据
+        entityCopy[key] = unflatten(entityCopy[key], innerschema)
+      }
+    })
+    // 依次存入
+    cache[schema.getName()][id] = entityCopy
+  }
+  return cache[schema.getName()][id]
+}
 
 // 针对schema是一个非EntitySchema实体的情况
 const unflattenNoEntity = (schema, input, unflatten) => {
-    const object = { ...input };
-    const arr = [];
-    let flag = schema instanceof Array;
-    //针对数组和非数组的情况进行判别
-    Object.keys(schema).forEach((key) => {
-        if (flag) {
-            if (object[key]) {
-                object[key] = unflatten(object[key], schema[key]);
-            }
-            arr.push(unflatten(object[key], schema[key]))
-        } else {
-            if (object[key]) {
-                object[key] = unflatten(object[key], schema[key]);
-            }
-        }
-    });
-    return flag ? arr : object
-};
+  const object = { ...input }
+  const arr = []
+  const flag = schema instanceof Array
+  // 判别数组和非数组，不同处理方法
+  Object.keys(schema).forEach((key) => {
+    if (flag) {
+      if (object[key]) {
+        object[key] = unflatten(object[key], schema[key])
+      }
+      arr.push(unflatten(object[key], schema[key]))
+    } else {
+      if (object[key]) {
+        object[key] = unflatten(object[key], schema[key])
+      }
+    }
+  })
+  return flag ? arr : object
+}
 
 // 传入entities,获取对应schema的某个id所对应的对象
 const getEntities = (entities) => {
-    return (entityOrId, schema) => {
-        const schemaKey = schema.getName();
-        if (typeof entityOrId === 'object') {
-            return entityOrId;
-        }
-        return entities[schemaKey][entityOrId];
-    };
-};
+  return (entityOrId, schema) => {
+    const schemaKey = schema.getName()
+    if (typeof entityOrId === 'object') {
+      return entityOrId
+    }
+    // 如果是id就返回entities中对应的范式化数据
+    return entities[schemaKey][entityOrId]
+  }
+}
 
 const schema = {
-    Entity: EntitySchema
-};
+  Entity: EntitySchema
+}
 
-const denormalize = (result, schema, entities) => getUnflatten(entities)(result, schema);
+const denormalize = (result, schema, entities) => getUnflatten(entities)(result, schema)
 
 // Define user
 const user = new schema.Entity('users', {}, {
-    idAttribute: 'uid'
-});
+  idAttribute: 'uid'
+})
 // Define comments schema
 const comment = new schema.Entity('comments', {
-    commenter: user
-});
+  commenter: user
+})
 // Define article
 const article = new schema.Entity('articles', {
-    author: user,
-    comments: {
-        result: [comment]
-    }
-});
+  author: user,
+  comments: {
+    result: [comment]
+  }
+})
 
 const normalizedData = {
-    result: "123",
-    entities: {
-        "articles": {
-            "123": {
-                id: "123",
-                author: "1",
-                title: "My awesome blog post",
-                comments: {
-                    total: 100,
-                    result: ["324"]
-                }
-            }
-        },
-        "users": {
-            "1": { "uid": "1", "name": "Paul" },
-            "2": { "uid": "2", "name": "Nicole" }
-        },
-        "comments": {
-            "324": { id: "324", "commenter": "2" }
+  result: '123',
+  entities: {
+    articles: {
+      123: {
+        id: '123',
+        author: '1',
+        title: 'My awesome blog post',
+        comments: {
+          total: 100,
+          result: ['324']
         }
+      }
+    },
+    users: {
+      1: { uid: '1', name: 'Paul' },
+      2: { uid: '2', name: 'Nicole' }
+    },
+    comments: {
+      324: { id: '324', commenter: '2' }
     }
+  }
 }
 
 const { result, entities } = normalizedData
 
 const denormalizedData = denormalize(result, article, entities)
 console.log(JSON.stringify(denormalizedData))
-
-
-
--- src/entity.js
+++ src/entity.js
@@ -1,29 +1,31 @@
-export default class EntitySchema {//创建一个类
-    constructor(name, entityParams = {}, entityConfig = {}) {
-        const idAttribute = entityConfig.idAttribute || 'id'
-        //const idAttribute = id
-        this.name = name
-        this.idAttribute = idAttribute
-        this.init(entityParams)//有可能存在嵌套，所以需要在init方法中遍历，不能直接使用
+export default class EntitySchema {// 创建一个类
+  constructor (name, entityParams = {}, entityConfig = {}) {
+    const idAttribute = entityConfig.idAttribute || 'id'
+    this.name = name
+    this.idAttribute = idAttribute
+    this.init(entityParams)// 有可能存在嵌套，所以需要在init方法中遍历，不能直接使用
+  }
+
+  // 获取name
+  getName () {
+    return this.name
+  }
+
+  // 获取id
+  getId (input) {
+    const key = this.idAttribute
+    return input[key]// 取对应id值
+  }
+
+  // entityParams存在嵌套的情况，因此需要在init方法中遍历entityParams中的schema属性，向this.schema添加对应的嵌套schema实例
+  init (entityParams) {
+    if (!this.schema) {
+      this.schema = {}
     }
-    //获取name
-    getName () {
-        return this.name
+    for (const key in entityParams) {
+      if (entityParams.hasOwnProperty(key)) {
+        this.schema[key] = entityParams[key]
+      }
     }
-    //获取id
-    getId (input) {
-        let key = this.idAttribute
-        return input[key]//取对应id值
-    }
-    //entityParams存在嵌套的情况，因此需要在init方法中遍历entityParams中的schema属性，向this.schema添加对应的嵌套schema实例
-    init (entityParams) {
-        if (!this.schema) {
-            this.schema = {}
-        }
-        for (let key in entityParams) {
-            if (entityParams.hasOwnProperty(key)) {
-                this.schema[key] = entityParams[key]
-            }
-        }
-    }
+  }
 }
